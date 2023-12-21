b5aac1b148610198e43165c55f771d92555e9df4
alexlimadev1997 <alexlimadev1997@gmail.com>
2023-12-21 11:39:39 -0300

    perf: refactory code

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,18 +1,18 @@
-let personagem = {
-  habilidade: 0,
-  energia: 0,
-  sorte: 0,
-  equipamentos: ["Espada", "Lampião"],
-  ouro: 0,
-  provisões: 0,
+const character = {
+  hability: 0,
+  energy: 0,
+  luck: 0,
+  equips: ["Espada", "Lampião"],
+  gold: 0,
+  provisions: 0,
 };
 
-const jogo = {
-  mundo: {
+const game = {
+  world: {
     _01: {
-      titulo: "1",
-      tipo: "historia",
-      texto: `Sua caminhada de dois dias finalmente chega ao fim. Você coloca sua espada no chão e senta-se nas pedras cobertas de musgo para descansar um 
+      tittle: "1",
+      type: "history",
+      text: `Sua caminhada de dois dias finalmente chega ao fim. Você coloca sua espada no chão e senta-se nas pedras cobertas de musgo para descansar um 
       pouco. Você espreguiça-se, esfrega os olhos e finalmente dá uma olhada na Montanha de Fogo. A própria montanha parece ameaçadora. A face escarpada 
       à sua frente parece ter sido rasgada pelas garras de alguma criatura gigantesca. Rochas afiadas projetam-se em ângulos não-naturais. No topo, você
       consegue ver a misteriosa coloração vermelha — provavelmente alguma estranha vegetação – que dá nome à montanha. Talvez ninguém jamais venha a saber
@@ -21,114 +21,114 @@ const jogo = {
       e frio. Você acende seu lampião e avança cautelosamente rumo à escuridão. Teias roçam seu rosto e você ouve passos apressados de pés pequenos:
       ratos, muito provavelmente. Você adentra a caverna. Depois de alguns metros, chega a uma bifurcação. Você vai para o oeste ou leste?
       `,
-      opcoes: { _71: "Para o Oeste", _278: "Para o Leste" },
+      buttons: { _71: "Para o Oeste", _278: "Para o Leste" },
     },
     _33: {
-      titulo: "33",
-      tipo: "historia",
-      texto: `A criatura acorda sobressaltada. Ela pula e corre na sua direção, desarmada. Com sua espada você deveria ser capaz de derrotá-la, mas seus
+      tittle: "33",
+      type: "history",
+      text: `A criatura acorda sobressaltada. Ela pula e corre na sua direção, desarmada. Com sua espada você deveria ser capaz de derrotá-la, mas seus
       dentes parecem perigosos. Você pode escapar pela porta(vá para 320) ou ficar e lutar com o orc. Se derrotá-lo, você pode pegar a caixa. Vá para 147.`,
-      opcoes: {
+      buttons: {
         _320: "Escapar pela porta",
         _lutar: { name: "orc", habilidade: 6, energia: 4 },
       },
     },
     _71: {
-      titulo: "71",
-      tipo: "teste",
-      texto: `Há uma curva na passagem para a direita, levando para o norte. Você se aproxima do posto de sentinela na esquina com cuidado e, ao olhar dentro 
+      tittle: "71",
+      type: "luckTest",
+      text: `Há uma curva na passagem para a direita, levando para o norte. Você se aproxima do posto de sentinela na esquina com cuidado e, ao olhar dentro 
       dele, vê uma criatura estranha, parecida com um goblin, trajando armadura de couro, dormindo em serviço. Você pode tentar passar furtivamente por ele. 
       Teste a sorte, Se for sortudo, ele não acorda e permanece roncando alto - Vá para 301. Se for Azarado, você pisa em algumas pedras soltas e os olhos 
       dele se abrem - Vá para a 248.`,
-      opcoes: { sorte: "Testar Sorte" },
+      buttons: { sorte: "Testar Sorte" },
       valores: ["_301", "_248"],
     },
     _82: {
-      titulo: "82",
-      tipo: "historia",
-      texto: `Você retorna para a bifurcação na passagem. À esquerda, você vê a entrada da à distância, mas segue em frente. Vá para a 71.`,
-      opcoes: { _71: "Para o Oeste" },
+      tittle: "82",
+      type: "history",
+      text: `Você retorna para a bifurcação na passagem. À esquerda, você vê a entrada da à distância, mas segue em frente. Vá para a 71.`,
+      buttons: { _71: "Para o Oeste" },
     },
     _92: {
-      titulo: "92",
-      tipo: "historia",
-      texto: `A porta abre, revelando um aposento pequeno e malcheiroso. No centro há uma mesa de madeira balouçante com uma vela acesa. Embaixo da mesa 
+      tittle: "92",
+      type: "history",
+      text: `A porta abre, revelando um aposento pequeno e malcheiroso. No centro há uma mesa de madeira balouçante com uma vela acesa. Embaixo da mesa 
       há uma pequena caixa de madeira. Dormindo em um colchão de palha em um canto mais afastado do aposento há uma criatura pequena, entroncada, com uma
-      cara feia e verruguenta, o mesmo tipo de criatura que você viu dormindo no posto de sentinela. Deve ser o guarda do turno da noite. Você pode retornar
+      cara feia e verruguenta, o mesmo type de criatura que você viu dormindo no posto de sentinela. Deve ser o guarda do turno da noite. Você pode retornar
       ao corredor e ir para o norte(vá para a 208) ou tentar atravessar furtivamente o aposento e tentar pegar a caixa sem despertar a criatura. Se quiser
       roubar a caixa, teste a sorte. Se for sortudo, ela não acorda – vá para a 147. Se for azarado, vá para a 33.`,
-      opcoes: { _208: "Para o norte", _testarSorte: ["_147", "_33"] },
+      buttons: { _208: "Para o norte", _testarSorte: ["_147", "_33"] },
     },
     _156: {
-      titulo: "156",
-      tipo: "teste",
+      tittle: "156",
+      type: "habilityTest",
 
-      texto: `Você dá um encontrão na porta. Role dois dados. Se o resultado for menor ou igual a sua Habilidade, você é bem-sucedido – vá para 343. 
+      text: `Você dá um encontrão na porta. Role dois dados. Se o resultado for menor ou igual a sua Habilidade, você é bem-sucedido – vá para 343. 
       Se o resultado for maior que a sua Habilidade, você se fere e decide não tentar de novo. Volte para 92.
       `,
-      opcoes: { testarHabilidade: "Testar Habilidade" },
+      buttons: { testarHabilidade: "Testar Habilidade" },
       valores: ["_343", "_92"],
     },
     _248: {
-      titulo: "248",
-      tipo: "historia",
-      texto: `A criatura que acabou de acordar é um orc! Ele se põe de pé lentamente e se vira para puxar uma corda que provavelmente é um alarme. Você precisa atacá-lo rapidamente.
+      tittle: "248",
+      type: "history",
+      text: `A criatura que acabou de acordar é um orc! Ele se põe de pé lentamente e se vira para puxar uma corda que provavelmente é um alarme. Você precisa atacá-lo rapidamente.
       Se o derrotar, pode continuar subindo a passagem - vá para 301.
       `,
-      opcoes: { _Batalha: "Batalhar" },
+      buttons: { _Batalha: "Batalhar" },
       _lutar: { name: "orc", habilidade: 6, energia: 4 },
     },
     _278: {
-      titulo: "278",
-      tipo: "historia",
-      texto: `A passagem logo termina em uma porta de madeira trancada. Você cola o ouvido na porta mas não ouve nada. Se quiser tentar derrubar a porta, 
+      tittle: "278",
+      type: "history",
+      text: `A passagem logo termina em uma porta de madeira trancada. Você cola o ouvido na porta mas não ouve nada. Se quiser tentar derrubar a porta, 
       vá para a 156. Se preferir voltar à bifurcação, vá para 92.`,
-      opcoes: { _156: "Tentar derrubar a porta", _92: "Voltar a Bifurcação" },
+      buttons: { _156: "Tentar derrubar a porta", _92: "Voltar a Bifurcação" },
     },
     _301: {
-      titulo: "301",
-      tipo: "historia",
-      texto: `A sua esquerda, no lado oeste da passagem, há uma porta de madeira rudimentar. Você cola o ouvido na porta e ouve um barulho que pode ser de
+      tittle: "301",
+      type: "history",
+      text: `A sua esquerda, no lado oeste da passagem, há uma porta de madeira rudimentar. Você cola o ouvido na porta e ouve um barulho que pode ser de
        alguma criatura roncando. Você quer abrir a porta? Nesse caso vá para a 82. Se quiser continuar para o norte, vá para 208.`,
-      opcoes: { _82: "Abrir a porta", _208: "Ir para o norte" },
+      buttons: { _82: "Abrir a porta", _208: "Ir para o norte" },
     },
     _343: {
-      titulo: "343",
-      tipo: "dano",
-      texto: `A porta cede e você cai de cabeça no aposento. Mas seu coração fica apertado quando você se dá conta de que não vai cair no chão, 
+      tittle: "343",
+      type: "damage",
+      text: `A porta cede e você cai de cabeça no aposento. Mas seu coração fica apertado quando você se dá conta de que não vai cair no chão, 
       mas em um poço! Por sorte, o poço não é muito fundo e você cai por mais ou menos dois metros. Perca 1 ponto de Energia devido aos ferimentos, 
       escale o poço e saia pela porta rumo ao oeste. Vá para a 92..
       `,
       dano: { energia: -1 },
-      opcoes: { _92: "Ir para a 92" },
+      buttons: { _92: "Ir para a 92" },
     },
   },
 };
 
-function criarTexto(value) {
-  for (local in jogo.mundo) {
+function createText(value) {
+  for (local in game.world) {
     if (local === value) {
-      document.querySelector(".text").textContent = jogo.mundo[local].texto;
-      document.querySelector(".title").textContent = jogo.mundo[local].titulo;
-      criarBotoes(
-        Object.values(jogo.mundo[local].opcoes).length,
-        Object.values(jogo.mundo[local].opcoes),
-        Object.keys(jogo.mundo[local].opcoes),
-        jogo.mundo[local].tipo,
-        jogo.mundo[local].valores
+      document.querySelector(".text").textContent = game.world[local].text;
+      document.querySelector(".title").textContent = game.world[local].tittle;
+      createButtons(
+        Object.values(game.world[local].buttons).length,
+        Object.values(game.world[local].buttons),
+        Object.keys(game.world[local].buttons),
+        game.world[local].type,
+        game.world[local].valores
       );
     }
   }
 }
 
-function criarBotoes(qtd, values, keys, type, valuesTest) {
+function createButtons(qtd, values, keys, type, valuesTest) {
   const actionsButtons = document.querySelector(".buttons");
   actionsButtons.innerHTML = "";
-  if (type === "historia") {
+  if (type === "history") {
     if (qtd == 1) {
       const button1 = document.createElement("button");
       button1.textContent = values[0];
-      button1.addEventListener("click", () => criarTexto(keys[0]));
+      button1.addEventListener("click", () => createText(keys[0]));
       actionsButtons.appendChild(button1);
     }
     if (qtd == 2) {
@@ -136,54 +136,66 @@ function criarBotoes(qtd, values, keys, type, valuesTest) {
       const button2 = document.createElement("button");
       button1.textContent = values[0];
       button2.textContent = values[1];
-      button1.addEventListener("click", () => criarTexto(keys[0]));
-      button2.addEventListener("click", () => criarTexto(keys[1]));
+      button1.addEventListener("click", () => createText(keys[0]));
+      button2.addEventListener("click", () => createText(keys[1]));
       actionsButtons.appendChild(button1);
       actionsButtons.appendChild(button2);
     }
-
-  } else if (type === "teste") {
+  } else if (type === "luckTest") {
     if (qtd == 1) {
       const button1 = document.createElement("button");
       button1.textContent = values[0];
-      button1.addEventListener("click", () => testarSorte(valuesTest));
+      button1.addEventListener("click", () => testLuck(valuesTest));
       actionsButtons.appendChild(button1);
     }
-
   } else if (type === "dano") {
-    personagem.energia -= 1;
-    document.querySelector(".energy").textContent = personagem.energia;
+    character.energia -= 1;
+    document.querySelector(".energy").textContent = character.energy;
     if (qtd == 1) {
       const button1 = document.createElement("button");
       button1.textContent = values[0];
-      button1.addEventListener("click", () => criarTexto(keys[0]));
+      button1.addEventListener("click", () => createText(keys[0]));
       actionsButtons.appendChild(button1);
     }
   }
+
+  if (type === "history") {
+  }
+
+  if (type === "luckTest") {
+  }
+
+  if (type == "habilityTest") {
+  }
+
+  if (type === "battle") {
+  }
+
+  if (type === "damage") {
+  }
 }
 
-function testarSorte(value) {
+function testLuck(value) {
   let dices = rolar_1d6() + rolar_1d6();
-  if (dices <= personagem.sorte) {
-    criarTexto(value[0]);
-    personagem.sorte--;
-    document.querySelector(".luckGame").textContent = personagem.sorte;
-
+  if (dices <= character.luck) {
+    createText(value[0]);
+    character.luck--;
+    document.querySelector(".luckGame").textContent = character.luck;
   } else {
-    criarTexto(value[1]);
-    personagem.sorte--;
-    document.querySelector(".luckGame").textContent = personagem.sorte;
+    createText(value[1]);
+    character.luck--;
+    document.querySelector(".luckGame").textContent = character.luck;
   }
   console.log(value[0], value[1]);
 }
 
 function testarHabilidade(value) {
   let dices = rolar_1d6() + rolar_1d6();
-  if (dices <= personagem.habilidade) {
-    criarTexto(value[0]);
+  if (dices <= character.habilidade) {
+    createText(value[0]);
   } else {
-    criarTexto(value[1]);
+    createText(value[1]);
   }
 }
 
-criarTexto("_01");
+createText("_01");
